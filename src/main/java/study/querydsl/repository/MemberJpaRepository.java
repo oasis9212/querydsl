15fef4503ea21a47bc82ceafb0d0af914ac1b99f
lostfly2 <lostfly2@naver.com>
2024-05-22 22:06:27 +0900

    쿼리 dsl 활용 실전. 및 api 예제.

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,16 +1,27 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.*;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -48,5 +59,79 @@ public class MemberJpaRepository {
         return factory.selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
+    }
+
+    public List<MemberTeamDto> searchBuilder(MemberSearchCondition condition){
+
+        BooleanBuilder builder= new BooleanBuilder();
+        if(hasText(condition.getUsername())){
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if(hasText(condition.getTeamName())){
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if(condition.getAgeGoe() !=null){
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if(condition.getAgeLoe() !=null){
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return  factory.select(new QMemberTeamDto(
+                    member.id.as("memberId"),
+                    member.username,
+                    member.age,
+                    team.id.as("teamId"),
+                    team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+
+    }
+
+
+    public List<MemberTeamDto> search(MemberSearchCondition con){
+
+
+        return  factory.select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(con.getUsername()),
+                        teamNameEq(con.getTeamName()),
+                        ageGoe(con.getAgeGoe()),
+                        ageLoe(con.getAgeLoe())
+
+                )
+                .fetch();
+
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return isEmpty(ageGoe) ?  null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return isEmpty(ageLoe) ?  null : member.age.loe(ageLoe);
+    }
+
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe){
+        return ageGoe(ageLoe).and(ageGoe(ageGoe));
     }
 }
--- src/test/java/study/querydsl/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/MemberJpaRepositoryTest.java
@@ -3,12 +3,19 @@ package study.querydsl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 import study.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,4 +38,37 @@ public class MemberJpaRepositoryTest {
 
         memberJpaRepository.findAll_queryDsl();
     }
+
+
+    @Test
+    public void searchtest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1= new Member("member1",10,teamA);
+        Member member2= new Member("member2",20,teamA);
+        Member member3= new Member("member3",30,teamB);
+        Member member4= new Member("member4",40,teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        MemberSearchCondition con=  new MemberSearchCondition();
+        con.setAgeGoe(35);
+        con.setAgeLoe(40);
+        con.setTeamName("teamB");
+
+        List<MemberTeamDto> memberTeamDtos = memberJpaRepository.search(con);
+
+        memberTeamDtos.stream().forEach(e -> System.out.println(e));
+    }
+
+
 }
